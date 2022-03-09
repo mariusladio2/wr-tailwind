4309f15cf4a053c100ec1e30ee49630d54995406
Marius Ladio II <mariusladio2@gmail.com>
2022-03-10 07:09:01 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  presets: [
-    require('./wr-preset.js')
-  ],
+theme: {
+    colors: {
+      'wr-pink': {
+          50: '#FFF0F2',
+          100: '#FFD6DD',
+          200: '#FFBDC8',
+          300: '#FFA3B3',
+          400: '#FF8A9D',
+          500: '#FF7088',
+          600: '#FF5773',
+          700: '#CC314B',
+          800: '#B21933',
+          900: '#99061F',
+      }
+    }
+  }
 }
