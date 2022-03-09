46b68b4617875bd2f18e70c50bb97c0e2b563d13
Marius Ladio II <mariusladio2@gmail.com>
2022-03-10 06:53:34 +0800

    Create wr-preset.js

---
+++ wr-preset.js
@@ -0,0 +1,19 @@
+// Well Revolution preset
+module.exports = {
+  theme: {
+    colors: {
+      'wr-pink': {
+          50: '#FFF0F2',
+          100: '#FFD6DD',
+          200: '#FFBDC8',
+          300: '#FFA3B3',
+          400: '#FF8A9D',
+          500: '#FF7088',
+          600: '#FF5773',
+          700: '#CC314B',
+          800: '#B21933',
+          900: '#99061F',
+        },
+      }
+    }
+  }
